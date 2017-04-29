4c0b544811b0e5023db61bc35af7148643486dea
Tim Scanlin <tscanlin@gmail.com>
2017-04-29 16:11:57 -0700

    reorganize

--- components/ScriptAdder/index.js
+++ components/ScriptAdder/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { addScriptsToPage } from '../../utils/add-script.js'
+import { addScriptsToPage } from '../utils/add-script.js'
 import * as SCRIPTS from './scripts.js'
 
 class ScriptAdder extends React.Component {
