4025cb33205a516871324d6931630a1c3f18a2e9
David J. Malan <malan@harvard.edu>
2018-09-02 17:05:35 -0400

    Checking for options.orderedList

--- src/js/build-html.js
+++ src/js/build-html.js
@@ -101,7 +101,8 @@ module.exports = function (options) {
    * @return {HTMLElement}
    */
   function createList (isCollapsed) {
-    var list = document.createElement('ol')
+    var listElement = (options.orderedList === false) ? 'ul' : 'ol'
+    var list = document.createElement(listElement)
     var classes = options.listClass +
       SPACE_CHAR + options.extraListClasses
     if (isCollapsed) {
