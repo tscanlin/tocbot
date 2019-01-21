03c20251621e4d90f3b4c31fd6604a81cfa041f6
Bo <BoPeng@users.noreply.github.com>
2019-01-21 00:49:09 -0600

    Fix duplicated ID (#122)
    * Fix duplicated ID
    
    * Update src/utils/make-ids.js
    
    Co-Authored-By: BoPeng <BoPeng@users.noreply.github.com>

--- src/utils/make-ids.js
+++ src/utils/make-ids.js
@@ -3,14 +3,12 @@ var headings = content.querySelectorAll('h1, h2, h3, h4, h5, h6, h7')
 var headingMap = {}
 
 Array.prototype.forEach.call(headings, function(heading) {
-  if (!heading.id) {
-    var id = heading.textContent.toLowerCase()
-      .split(' ').join('-').split(':').join('')
-    headingMap[id] = !isNaN(headingMap[id]) ? headingMap[id]++ : 0
-    if (headingMap[id]) {
-      heading.id = id + '-' + headingMap[id]
-    } else {
-      heading.id = id
-    }
+  var id = heading.id ? heading.id : heading.textContent.toLowerCase()
+      .split(' ').join('-').split(':').join('');
+  headingMap[id] = !isNaN(headingMap[id]) ? ++headingMap[id] : 0;
+  if (headingMap[id]) {
+    heading.id = id + '-' + headingMap[id]
+  } else {
+    heading.id = id
   }
 })
