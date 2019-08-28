56ff3e9c5b13d6e83311567824328000ca629053
Tim Scanlin <tscanlin@users.noreply.github.com>
2019-08-27 18:42:45 -0700

    Update build-html.js

--- src/js/build-html.js
+++ src/js/build-html.js
@@ -205,7 +205,7 @@ module.exports = function (options) {
       var activeTocLink = document.querySelector(options.tocSelector)
         .querySelector('.' + options.linkClass +
           '.node-name--' + topHeader.nodeName +
-          '[href="#' + topHeader.id + '"]')
+          '[href*="#' + topHeader.id + '"]')
       if (activeTocLink.className.indexOf(options.activeLinkClass) === -1) {
         activeTocLink.className += SPACE_CHAR + options.activeLinkClass
       }
