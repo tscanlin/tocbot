2d5f52748eb3edf299bc189734749a5004bd07be
Tim Scanlin <tscanlin@gmail.com>
2024-05-21 16:33:24 -0700

    remove example code

--- src/js/server-render.js
+++ src/js/server-render.js
@@ -1,12 +1,6 @@
 import * as tocbot from './index-esm.js'
 import { JSDOM } from 'jsdom'
 
-const HTML_EXAMPLE = `
-<h1 id="test">Test</h1>
-<p>Content</p>
-<h2 id="next-heading">Next Heading</h1>
-`
-
 export function htmlTemplate (content){
   return `
 <html>
@@ -33,6 +27,3 @@ export function serverRender (content) {
   const toc = window.document.body.querySelector('.js-toc')
   return toc && toc.innerHTML
 }
-
-const result = serverRender(HTML_EXAMPLE)
-console.log(result)
